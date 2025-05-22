b596cef7c3e57dc30b5737d17b2967d8c4f77970
anandkushwaha <ait.17bcs388@gmail.com>
2025-05-22 22:03:38 +0530

    models

--- myapp1/models.py
+++ myapp1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Feature:
+    id: int
+    heading: str
+    subHeading:str
+    desc: str
--- myapp1/views.py
+++ myapp1/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Feature
 
 # Create your views here.
 
 def index(request):
-    context = {
-        'name': "Anand",
-        "age": "21",
-        'nationality':"Indian"
-    }
-    return render(request, 'index.html')
+    feature1= Feature()
+    feature1.id=0;
+    feature1.heading='Liberty NFT Market'
+    feature1.subHeading='Create, Sell & Collect Top NFT'
+    feature1.desc="Liberty NFT Market is a really cool and professional design for your NFT websites. This HTML CSS template is based on Bootstrap v5 and it is designed for NFT related web portals. Liberty can be freely downloaded from TemplateMo's free css templates"
+    return render(request, 'index.html', {'feature':feature1})
 
 
 def counter(request):

3756488f188c3f1e9800dcfbc8fd6419449759af
anandkushwaha <ait.17bcs388@gmail.com>
2025-05-23 15:40:40 +0530

    login and logout

--- myapp1/views.py
+++ myapp1/views.py
@@ -41,7 +41,8 @@ def register(request):
                 return redirect('register')
             else:
                 user= User.objects.create_user(username=username, email=email, password=password)
-                user.save();
+                user.save()
+                return redirect('login')
                 
         else:
             messages.info(request, 'Password is not same')
@@ -51,3 +52,26 @@ def register(request):
     else:
         return render(request, 'register.html')
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+    
+   
+   
+   
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
